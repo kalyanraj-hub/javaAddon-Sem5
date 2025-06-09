23733c5700b0a317d92ce8633837e785321d6f98
kalyanraj-hub <2322jc29@kgcas.com>
2025-06-09 15:19:37 +0530

    Create superclass.java

---
+++ superclass.java
@@ -0,0 +1,48 @@
+package Oops;
+
+class superclass{
+
+	void method1() {
+
+		System.out.println("subclass ");
+
+	}
+
+	
+
+	
+
+}
+
+
+
+class subclass1 extends superclass{
+
+	void method2() {
+
+		System.out.println("subclass 1");
+
+	}
+
+	
+
+	
+
+}
+
+
+
+class subclass2 extends superclass{
+
+	void method3() {
+
+		System.out.println("subclass 2");
+
+	}
+
+	
+
+	
+
+}
+
