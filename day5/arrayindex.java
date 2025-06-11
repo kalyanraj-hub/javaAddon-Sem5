2707334210787275671dcb1ed02ef86cedd011ec
kalyanraj-hub <2322jc29@kgcas.com>
2025-06-11 11:58:28 +0530

    Add files via upload

---
+++ day5/arrayindex.java
@@ -0,0 +1,40 @@
+package java_addon;
+
+public class main {
+
+
+
+	public static void main(String[] args) {
+
+		try{
+
+			int[] myNumber = {1,2,3};
+
+		
+
+		System.out.println(myNumber[10]);
+
+		}
+
+		catch( Exception e) {
+
+			System.out.println("The lenght is mismatch");
+
+			
+
+		}
+
+		finally {
+
+			System.out.println(" invald input");
+
+		}
+
+		
+
+	}
+
+
+
+}
+
