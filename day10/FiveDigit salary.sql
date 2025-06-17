5de79631b4a40b2dc22c41f41fdfd60405f069f2
kalyanraj-hub <2322jc29@kgcas.com>
2025-06-17 10:25:08 +0530

    Create FiveDigit salary.sql

---
+++ day10/FiveDigit salary.sql
@@ -0,0 +1,11 @@
+mysql> select name,salary
+    -> from Employee
+    -> where salary like'_____';
++-------+--------+
+| name  | salary |
++-------+--------+
+| henry |  20000 |
++-------+--------+
+1 row in set (0.03 sec)
+
+mysql>
