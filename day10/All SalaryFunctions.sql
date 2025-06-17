775818704ea05dc158c81b267792d21a6d382e2a
kalyanraj-hub <2322jc29@kgcas.com>
2025-06-17 12:16:04 +0530

    Create All SalaryFunctions.sql

---
+++ day10/All SalaryFunctions.sql
@@ -0,0 +1,19 @@
+mysql> select avg(salary),sum(salary),max(salary)
+    -> from Employee;
++-------------+-------------+-------------+
+| avg(salary) | sum(salary) | max(salary) |
++-------------+-------------+-------------+
+| 284000.0000 |     1420000 |     1000000 |
++-------------+-------------+-------------+
+1 row in set (0.00 sec)
+
+mysql> select avg(salary),sum(salary),max(salary),count(name)
+    -> from Employee;
++-------------+-------------+-------------+-------------+
+| avg(salary) | sum(salary) | max(salary) | count(name) |
++-------------+-------------+-------------+-------------+
+| 284000.0000 |     1420000 |     1000000 |           5 |
++-------------+-------------+-------------+-------------+
+1 row in set (0.00 sec)
+
+mysql>
