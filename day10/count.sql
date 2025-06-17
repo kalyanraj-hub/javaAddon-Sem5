cb6a5175fec4defa99077c8c6b9b3368df3eeac5
kalyanraj-hub <2322jc29@kgcas.com>
2025-06-17 12:03:28 +0530

    Create count.sql

---
+++ day10/count.sql
@@ -0,0 +1,11 @@
+mysql> select count(*)
+    -> from Employee
+    -> where salary<2000 and empid =5;
++----------+
+| count(*) |
++----------+
+|        0 |
++----------+
+1 row in set (0.04 sec)
+
+mysql>
