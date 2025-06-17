c80381726c1cb0d01303891e4c9c4f4bb29ca65a
kalyanraj-hub <2322jc29@kgcas.com>
2025-06-17 12:07:09 +0530

    Create sum Salary.sql

---
+++ day10/sum Salary.sql
@@ -0,0 +1,11 @@
+mysql> select sum(salary)
+    -> from Employee
+    -> where dep='finance';
++-------------+
+| sum(salary) |
++-------------+
+|      200000 |
++-------------+
+1 row in set (0.04 sec)
+
+mysql>
