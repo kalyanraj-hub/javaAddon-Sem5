38b97a04e5b60c656e355e6f1deff449ba490d00
kalyanraj-hub <2322jc29@kgcas.com>
2025-06-17 10:39:58 +0530

    Create findNAME using id.sql

---
+++ day10/findNAME using id.sql
@@ -0,0 +1,14 @@
+mysql> select name
+    -> from Employee
+    -> where empid in (1,2,3,4);
++----------+
+| name     |
++----------+
+| soma     |
+| chandran |
+| henry    |
+| kathir   |
++----------+
+4 rows in set (0.00 sec)
+
+mysql>
